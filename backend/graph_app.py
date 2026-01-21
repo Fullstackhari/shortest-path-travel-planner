2f13738c5d60562cc8085c1b7e7ab5dadc4b6f97
C Harindra <122661512+Fullstackhari@users.noreply.github.com>
2026-01-21 14:09:44 +0530

    Update graph_app.py

--- backend/graph_app.py
+++ backend/graph_app.py
@@ -64,9 +64,13 @@ except Exception as e:
 # Database Helpers
 # -------------------------------
 def get_db_connection():
-    conn = sqlite3.connect("travel_planner_users.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DB_PATH = os.path.join(BASE_DIR, "travel_planner_users.db")
+
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def init_db():
     conn = get_db_connection()
